98262a052ecb6e7f97c06b515347016dd3cb0ff0
Fuqiang Wu <wufuqiang@buaa.edu.cn>
2021-12-16 00:04:10 +0800

    ADD:go语言学习

---
+++ src/go_code/project1/processControl/switch/switchDemo.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var char byte
+	fmt.Println("请输入一个字符：")
+	fmt.Scanf("%c", &char)
+
+	switch char {
+	case 'a':
+		fmt.Println("A")
+	case 'b':
+		fmt.Println("B")
+	default:
+		fmt.Println("Other")
+	}
+}
