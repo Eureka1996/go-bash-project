16f09d90e76a13a3631796483b4494dde72d30f2
wufuqiang <wufuqiang@buaa.edu.cn>
2022-06-19 18:23:26 +0800

    feat: timer

---
+++ src/go_code/my_timer/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "go_base/src/go_code/my_timer/timer_utils"
+
+/**
+ * @author wufuqiang
+ * @date 2022/6/5 21:57
+ */
+
+func main() {
+	//timer_utils.TimerTest1()
+	timer_utils.TimerTest2()
+}
---
+++ src/go_code/my_timer/timer_utils/TimerUtils.go
@@ -0,0 +1,48 @@
+package timer_utils
+
+import (
+	"fmt"
+	"time"
+)
+
+/**
+ * @author wufuqiang
+ * @date 2022/6/5 21:52
+ */
+// 阻塞指定时间
+func TimerTest1() {
+	timer := time.NewTimer(time.Second * 10)
+	fmt.Println("当前时间：", time.Now())
+	t1 := <-timer.C // 阻塞的，指定时间到
+	fmt.Println("t1:", t1)
+
+	// 如果只是单纯的等待，可以使用time.Sleep来实现。
+	timer2 := time.NewTimer(time.Second * 5)
+	<-timer2.C
+	fmt.Println("5s之后")
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("再一次5s后")
+
+	<-time.After(time.Second * 5) // time.After函数的返回值是chan Time
+	fmt.Println("再再一次5s后")
+
+}
+
+func TimerTest2() {
+	timer := time.NewTimer(time.Second * 2)
+
+	go func() {
+		<-timer.C
+		fmt.Println("Timer expired")
+	}()
+
+	stop := timer.Stop()
+
+	if stop {
+		fmt.Println("Timer stopped")
+	}
+
+	<-time.After(time.Second * 5)
+	fmt.Println("主方法")
+}
